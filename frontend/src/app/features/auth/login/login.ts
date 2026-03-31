dc8067b1e4a1e64c30c33dc33a4f7e6da9c3e5bf
hsibtain18 <33388697+hsibtain18@users.noreply.github.com>
2026-03-31 16:45:03 +0400

    frontend: login

--- frontend/src/app/features/auth/login/login.ts
+++ frontend/src/app/features/auth/login/login.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  loading = signal(false);
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  submit() {
+    if (this.form.invalid) return;
+
+    this.loading.set(true);
+
+    setTimeout(() => {
+      console.log(this.form.value);
+      this.loading.set(false);
+    }, 1500);
+  }
+}
